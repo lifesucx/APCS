d30ee25c821fdd8b260e5a020ee9f43d19ef56b1
Namo <bentlypamela@gmail.com>
2019-04-16 13:22:36 -0700

    adding main class and printCards

--- Cardgame/src/Deck.java
+++ Cardgame/src/Deck.java
@@ -1,20 +1,29 @@
 import java.util.ArrayList;
 
-public class Deck{
+public class Deck {
     // private static final int[] Deck = null;
-    private ArrayList<Card> deck;
     private ArrayList<Card> discard;
-    public Deck(){
+    public static void main(String[] args) {
+        new Deck().printCards();
+    }
+
+    private ArrayList<Card> deck;
+
+
+    // private String res() {
+    // }
+
+    public Deck() {
         //initializing deck and discard
         this.deck = new ArrayList<>();
         this.discard = new ArrayList<>();
         //creating the actual deck
-        for(int suit = Card.CLUBS; suit <= Card.SPADES; suit++){
-            for(int rank = Card.ACE; rank <= Card.KING; rank++){
+        for (int suit = Card.CLUBS; suit <= Card.SPADES; suit++) {
+            for (int rank = Card.ACE; rank <= Card.KING; rank++) {
                 deck.add(new Card(rank, suit));
             }
+        }
     }
-}
 
     //This is what makes the add work
     // private int[] add(int[] card, int[] Deck) {
@@ -22,7 +31,23 @@ public class Deck{
     //}
 
     //next we need the shuffle class
-    public static shuffleDeck(){
+    // public static shuffleDeck(){
 
+    // }
+
+    public void printCards() {
+        System.out.println(deck);
     }
+
+
+    public Card dealCard(){
+        if(deck.size() > 0){
+            Card res = deck.remove(0);
+            discard.add(res);
+            return res;
+        }else{
+            return res;
+        }
+    }
+
 }
