cef42abd4e59fc60dd8175dd7377cffcc65cf503
Namo <bentlypamela@gmail.com>
2019-04-17 09:46:11 -0700

    Adding the player class, and more explained comments in the Deck class

--- Cardgame/src/Deck.java
+++ Cardgame/src/Deck.java
@@ -1,18 +1,16 @@
 import java.util.ArrayList;
 
 public class Deck {
-    // private static final int[] Deck = null;
+    //The fields
     private ArrayList<Card> discard;
+    private ArrayList<Card> deck;
+
+    //main class, makes it possible to test the print cards class
     public static void main(String[] args) {
         new Deck().printCards();
     }
 
-    private ArrayList<Card> deck;
-
-
-    // private String res() {
-    // }
-
+    //Creating a deck that will create the deck in order
     public Deck() {
         //initializing deck and discard
         this.deck = new ArrayList<>();
@@ -25,21 +23,13 @@ public class Deck {
         }
     }
 
-    //This is what makes the add work
-    // private int[] add(int[] card, int[] Deck) {
-    //     return null;
-    //}
-
-    //next we need the shuffle class
-    // public static shuffleDeck(){
-
-    // }
-
+    //prints the deck
+    //mainly to test the deck class works
     public void printCards() {
         System.out.println(deck);
     }
 
-
+    //deals the card that is on top
     public Card dealCard(){
         if(deck.size() > 0){
             Card res = deck.remove(0);
@@ -48,6 +38,11 @@ public class Deck {
         }else{
             return res;
         }
+    }
+
+    //shuffles the cards
+    private void shuffle(){
+
     }
 
 }
---
+++ Cardgame/src/Player.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Player {
+    //The fields
+    private ArrayList<Card> hand;
+    private int rounds;
+    private int wins;
+
+    public static void main(String[] args){
+
+    }
+
+    //Prints the amount of points the player has
+    private int getPoints(){
+
+    }
+
+    //Prints the cards the player and computer both have
+    private void printCards(){
+
+    }
+
+    //Prints the value of player and computers cards
+    private int getHandValue(){
+
+    }
+}
